e053724cf4a5f699f68033aca7c61db16af8780b
Frank Agnello <frankjagnello@icloud.com>
2022-12-16 19:56:33 -0500

    Test

---
+++ poker/card/Card.java
@@ -0,0 +1,20 @@
+package poker.card;
+
+import poker.card.*;
+
+public class Card {
+    public Suit suit;
+    public Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+    public String toString() {
+        return value + " " + suit;
+    }
+}
